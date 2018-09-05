d38a8b2ee94b17e1d0c3c8843db9bd8200b5176c
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-05 02:02:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,10 +5,8 @@ var instruments = ["guitar", "guitar", "bass", "drums"];
 function theBeatlesPlay(musicians, instruments) {
   var array = [];
   for (let i = 0; i < 4; i++) {
-      array.push(`${musicians[0]} plays ${instruments[0]}`);
-      array.push(`${musicians[1]} plays ${instruments[1]}`);
-      array.push(`${musicians[2]} plays ${instruments[2]}`);
-      array.push(`${musicians[3]} plays ${instruments[3]}`);
+      array.push(`${musicians[i]} plays ${instruments[i]}`);
+
     }
   return array
 }
