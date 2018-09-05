8a21a9ebf46632987490d20543994628e129ad8b
sadler2007@gmail.com <sadler2007@gmail.com>
2018-09-05 02:01:11 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -10,16 +10,3 @@ function theBeatlesPlay(musicians, instruments) {
   return theBeatlesPlay(musicians, instruments);
 }
 
-
-const facts = [
-  "He was the last Beatle to learn to drive",
-  "He was never a vegetarian",
-  "He was a choir boy and boy scout",
-  "He hated the sound of his own voice"
-];
-
-function johnLennonFacts(facts) {
-  while (n) {
-    facts[n] + "!!!";
-  }
-  return facts;  
